a614cb3b8c0a31e6a79e104963d6576e1b62aca5
ghostl33t <alibegovicjasim1337@gmail.com>
2023-12-02 17:10:16 +0100

    7-generate-zone-related-entities Adding Zones table

--- TaxiHereAPI/Database/ApplicationDBContext.cs
+++ TaxiHereAPI/Database/ApplicationDBContext.cs
@@ -13,4 +13,5 @@ public class ApplicationDBContext : DbContext
     public DbSet<User> Users { get; set; }
     public DbSet<Country> Countries { get; set; }
     public DbSet<City> Cities { get; set; }
+    public DbSet<Zone> Zones { get; set; }
 }
