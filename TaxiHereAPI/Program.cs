20bb65c4b9cd077107054494722efee79bf99800
ghostl33t <alibegovicjasim1337@gmail.com>
2023-12-02 15:55:43 +0100

    3-database-context Setting Up database, adding configurations

--- TaxiHereAPI/Program.cs
+++ TaxiHereAPI/Program.cs
@@ -1,21 +1,48 @@
+using Microsoft.EntityFrameworkCore;
+using TaxiHereAPI.Database;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+#region EnvironmentSetup
+// Load environment-specific configuration (appsettings.{Environment}.json)
+builder.Configuration.AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true);
+#endregion
 
+#region Dependencies-Services
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+#endregion
+
+#region Database
+builder.Services.AddDbContext<ApplicationDBContext>(options =>
+{
+    var mainConnectionString = builder.Configuration.GetConnectionString("DBConnection");
+    if (mainConnectionString != null)
+    {
+        options.UseSqlServer(mainConnectionString);
+        Console.WriteLine("INFO: Connection with the database established successfuly!");
+    }
+    else
+    {
+        Console.WriteLine("ERROR: Unable to connect to the SQL server.");
+    }
+});
+#endregion
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+#region RunSwaggerUI
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+#endregion
 
+#region AppSettings
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
@@ -23,3 +50,4 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+#endregion
